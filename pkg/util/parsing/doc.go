6480a7d6bb4e1bf1f47b8206bbcc01929af62f89
ymqytw <mengqiy@google.com>
2017-08-30 14:11:54 -0700

    move pkg and drop no need files

--- pkg/util/parsing/doc.go
+++ pkg/util/parsing/doc.go
@@ -14,13 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package openapi
-
-import "github.com/go-openapi/spec"
-
-const PrintColumnsKey = "x-kubernetes-print-columns"
-
-// GetPrintColumns looks for the open API extension for the display columns.
-func GetPrintColumns(extensions spec.Extensions) (string, bool) {
-	return extensions.GetString(PrintColumnsKey)
-}
+// Package openapi is a collection of libraries for fetching the openapi spec
+// from a Kubernetes server and then indexing the type definitions.
+// The openapi spec contains the object model definitions and extensions metadata
+// such as the patchStrategy and patchMergeKey for creating patches.
+package parsing
--- pkg/util/parsing/openapi_test.go
+++ pkg/util/parsing/openapi_test.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package openapi_test
+package parsing_test
 
 import (
 	"path/filepath"
@@ -23,18 +23,18 @@ import (
 	. "github.com/onsi/gomega"
 
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/kubernetes/pkg/kubectl/cmd/util/openapi"
-	tst "k8s.io/kubernetes/pkg/kubectl/cmd/util/openapi/testing"
+	"k8s.io/kube-openapi/pkg/util/parsing"
+	tst "k8s.io/kube-openapi/pkg/util/parsing/testing"
 )
 
-var fakeSchema = tst.Fake{Path: filepath.Join("..", "..", "..", "..", "..", "api", "openapi-spec", "swagger.json")}
+var fakeSchema = tst.Fake{Path: filepath.Join("testing", "swagger.json")}
 
 var _ = Describe("Reading apps/v1beta1/Deployment from openAPIData", func() {
-	var resources openapi.Resources
+	var resources parsing.Resources
 	BeforeEach(func() {
 		s, err := fakeSchema.OpenAPISchema()
 		Expect(err).To(BeNil())
-		resources, err = openapi.NewOpenAPIData(s)
+		resources, err = parsing.NewOpenAPIData(s)
 		Expect(err).To(BeNil())
 	})
 
@@ -44,15 +44,15 @@ var _ = Describe("Reading apps/v1beta1/Deployment from openAPIData", func() {
 		Group:   "apps",
 	}
 
-	var schema openapi.Schema
+	var schema parsing.Schema
 	It("should lookup the Schema by its GroupVersionKind", func() {
 		schema = resources.LookupResource(gvk)
 		Expect(schema).ToNot(BeNil())
 	})
 
-	var deployment *openapi.Kind
+	var deployment *parsing.Kind
 	It("should be a Kind", func() {
-		deployment = schema.(*openapi.Kind)
+		deployment = schema.(*parsing.Kind)
 		Expect(deployment).ToNot(BeNil())
 	})
 
@@ -62,7 +62,7 @@ var _ = Describe("Reading apps/v1beta1/Deployment from openAPIData", func() {
 
 	It("should have a kind key of type string", func() {
 		Expect(deployment.Fields).To(HaveKey("kind"))
-		key := deployment.Fields["kind"].(*openapi.Primitive)
+		key := deployment.Fields["kind"].(*parsing.Primitive)
 		Expect(key).ToNot(BeNil())
 		Expect(key.Type).To(Equal("string"))
 		Expect(key.GetPath().Get()).To(Equal([]string{"io.k8s.api.apps.v1beta1.Deployment", ".kind"}))
@@ -70,7 +70,7 @@ var _ = Describe("Reading apps/v1beta1/Deployment from openAPIData", func() {
 
 	It("should have a apiVersion key of type string", func() {
 		Expect(deployment.Fields).To(HaveKey("apiVersion"))
-		key := deployment.Fields["apiVersion"].(*openapi.Primitive)
+		key := deployment.Fields["apiVersion"].(*parsing.Primitive)
 		Expect(key).ToNot(BeNil())
 		Expect(key.Type).To(Equal("string"))
 		Expect(key.GetPath().Get()).To(Equal([]string{"io.k8s.api.apps.v1beta1.Deployment", ".apiVersion"}))
@@ -78,50 +78,50 @@ var _ = Describe("Reading apps/v1beta1/Deployment from openAPIData", func() {
 
 	It("should have a metadata key of type Reference", func() {
 		Expect(deployment.Fields).To(HaveKey("metadata"))
-		key := deployment.Fields["metadata"].(openapi.Reference)
+		key := deployment.Fields["metadata"].(parsing.Reference)
 		Expect(key).ToNot(BeNil())
 		Expect(key.Reference()).To(Equal("io.k8s.apimachinery.pkg.apis.meta.v1.ObjectMeta"))
-		subSchema := key.SubSchema().(*openapi.Kind)
+		subSchema := key.SubSchema().(*parsing.Kind)
 		Expect(subSchema).ToNot(BeNil())
 	})
 
-	var status *openapi.Kind
+	var status *parsing.Kind
 	It("should have a status key of type Reference", func() {
 		Expect(deployment.Fields).To(HaveKey("status"))
-		key := deployment.Fields["status"].(openapi.Reference)
+		key := deployment.Fields["status"].(parsing.Reference)
 		Expect(key).ToNot(BeNil())
 		Expect(key.Reference()).To(Equal("io.k8s.api.apps.v1beta1.DeploymentStatus"))
-		status = key.SubSchema().(*openapi.Kind)
+		status = key.SubSchema().(*parsing.Kind)
 		Expect(status).ToNot(BeNil())
 	})
 
 	It("should have a valid DeploymentStatus", func() {
 		By("having availableReplicas key")
 		Expect(status.Fields).To(HaveKey("availableReplicas"))
-		replicas := status.Fields["availableReplicas"].(*openapi.Primitive)
+		replicas := status.Fields["availableReplicas"].(*parsing.Primitive)
 		Expect(replicas).ToNot(BeNil())
 		Expect(replicas.Type).To(Equal("integer"))
 
 		By("having conditions key")
 		Expect(status.Fields).To(HaveKey("conditions"))
-		conditions := status.Fields["conditions"].(*openapi.Array)
+		conditions := status.Fields["conditions"].(*parsing.Array)
 		Expect(conditions).ToNot(BeNil())
 		Expect(conditions.GetName()).To(Equal(`Array of Reference to "io.k8s.api.apps.v1beta1.DeploymentCondition"`))
 		Expect(conditions.GetExtensions()).To(Equal(map[string]interface{}{
 			"x-kubernetes-patch-merge-key": "type",
 			"x-kubernetes-patch-strategy":  "merge",
 		}))
-		condition := conditions.SubType.(openapi.Reference)
+		condition := conditions.SubType.(parsing.Reference)
 		Expect(condition.Reference()).To(Equal("io.k8s.api.apps.v1beta1.DeploymentCondition"))
 	})
 
-	var spec *openapi.Kind
+	var spec *parsing.Kind
 	It("should have a spec key of type Reference", func() {
 		Expect(deployment.Fields).To(HaveKey("spec"))
-		key := deployment.Fields["spec"].(openapi.Reference)
+		key := deployment.Fields["spec"].(parsing.Reference)
 		Expect(key).ToNot(BeNil())
 		Expect(key.Reference()).To(Equal("io.k8s.api.apps.v1beta1.DeploymentSpec"))
-		spec = key.SubSchema().(*openapi.Kind)
+		spec = key.SubSchema().(*parsing.Kind)
 		Expect(spec).ToNot(BeNil())
 	})
 
@@ -132,18 +132,18 @@ var _ = Describe("Reading apps/v1beta1/Deployment from openAPIData", func() {
 
 	It("should have a spec with a PodTemplateSpec sub-field", func() {
 		Expect(spec.Fields).To(HaveKey("template"))
-		key := spec.Fields["template"].(openapi.Reference)
+		key := spec.Fields["template"].(parsing.Reference)
 		Expect(key).ToNot(BeNil())
 		Expect(key.Reference()).To(Equal("io.k8s.api.core.v1.PodTemplateSpec"))
 	})
 })
 
 var _ = Describe("Reading authorization.k8s.io/v1/SubjectAccessReview from openAPIData", func() {
-	var resources openapi.Resources
+	var resources parsing.Resources
 	BeforeEach(func() {
 		s, err := fakeSchema.OpenAPISchema()
 		Expect(err).To(BeNil())
-		resources, err = openapi.NewOpenAPIData(s)
+		resources, err = parsing.NewOpenAPIData(s)
 		Expect(err).To(BeNil())
 	})
 
@@ -153,35 +153,35 @@ var _ = Describe("Reading authorization.k8s.io/v1/SubjectAccessReview from openA
 		Group:   "authorization.k8s.io",
 	}
 
-	var schema openapi.Schema
+	var schema parsing.Schema
 	It("should lookup the Schema by its GroupVersionKind", func() {
 		schema = resources.LookupResource(gvk)
 		Expect(schema).ToNot(BeNil())
 	})
 
-	var sarspec *openapi.Kind
+	var sarspec *parsing.Kind
 	It("should be a Kind and have a spec", func() {
-		sar := schema.(*openapi.Kind)
+		sar := schema.(*parsing.Kind)
 		Expect(sar).ToNot(BeNil())
 		Expect(sar.Fields).To(HaveKey("spec"))
-		specRef := sar.Fields["spec"].(openapi.Reference)
+		specRef := sar.Fields["spec"].(parsing.Reference)
 		Expect(specRef).ToNot(BeNil())
 		Expect(specRef.Reference()).To(Equal("io.k8s.api.authorization.v1.SubjectAccessReviewSpec"))
-		sarspec = specRef.SubSchema().(*openapi.Kind)
+		sarspec = specRef.SubSchema().(*parsing.Kind)
 		Expect(sarspec).ToNot(BeNil())
 	})
 
 	It("should have a valid SubjectAccessReviewSpec", func() {
 		Expect(sarspec.Fields).To(HaveKey("extra"))
-		extra := sarspec.Fields["extra"].(*openapi.Map)
+		extra := sarspec.Fields["extra"].(*parsing.Map)
 		Expect(extra).ToNot(BeNil())
 		Expect(extra.GetName()).To(Equal("Map of Array of string"))
 		Expect(extra.GetPath().Get()).To(Equal([]string{"io.k8s.api.authorization.v1.SubjectAccessReviewSpec", ".extra"}))
-		array := extra.SubType.(*openapi.Array)
+		array := extra.SubType.(*parsing.Array)
 		Expect(array).ToNot(BeNil())
 		Expect(array.GetName()).To(Equal("Array of string"))
 		Expect(array.GetPath().Get()).To(Equal([]string{"io.k8s.api.authorization.v1.SubjectAccessReviewSpec", ".extra"}))
-		str := array.SubType.(*openapi.Primitive)
+		str := array.SubType.(*parsing.Primitive)
 		Expect(str).ToNot(BeNil())
 		Expect(str.Type).To(Equal("string"))
 		Expect(str.GetName()).To(Equal("string"))
@@ -191,25 +191,25 @@ var _ = Describe("Reading authorization.k8s.io/v1/SubjectAccessReview from openA
 
 var _ = Describe("Path", func() {
 	It("can be created by NewPath", func() {
-		path := openapi.NewPath("key")
+		path := parsing.NewPath("key")
 		Expect(path.String()).To(Equal("key"))
 	})
 	It("can create and print complex paths", func() {
-		key := openapi.NewPath("key")
+		key := parsing.NewPath("key")
 		array := key.ArrayPath(12)
 		field := array.FieldPath("subKey")
 
 		Expect(field.String()).To(Equal("key[12].subKey"))
 	})
 	It("has a length", func() {
-		key := openapi.NewPath("key")
+		key := parsing.NewPath("key")
 		array := key.ArrayPath(12)
 		field := array.FieldPath("subKey")
 
 		Expect(field.Len()).To(Equal(3))
 	})
 	It("can look like an array", func() {
-		key := openapi.NewPath("key")
+		key := parsing.NewPath("key")
 		array := key.ArrayPath(12)
 		field := array.FieldPath("subKey")
 
