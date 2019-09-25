6742ec68c46f552b387ff92f0560e8f2573b10d7
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2019-09-25 13:57:13 +0200

    adds version and info fields

--- pkg/spec3/spec.go
+++ pkg/spec3/spec.go
@@ -5,10 +5,15 @@ package spec3
 // Note: at the moment this struct doesn't fully conforms to the OpenAPI Specification in version 3.0,
 //       it is just a proof of concept
 type OpenAPI struct {
+	// Version represents the semantic version number of the OpenAPI Specification that this document uses
+	Version string `json:"openapi"`
+
+	// Info provides metadata about the API
+	Info Info `json:"info"`
+
 	// Paths holds the available target and operations for the API
 	Paths *Paths `json:"paths,omitempty"`
 
 	// Components hold various schemas for the specification
 	Components *Components `json:"components,omitempty"`
 }
-
