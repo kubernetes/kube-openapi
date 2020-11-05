6022c636c9c7668d7e3e61eb5eb33d54223a7fa3
Gautier Delorme <gautier.delorme@gmail.com>
2020-11-05 13:51:11 +0100

    Fix unexpected error values
    Signed-off-by: Gautier Delorme <gautier.delorme@gmail.com>

--- pkg/validation/validate/object_validator.go
+++ pkg/validation/validate/object_validator.go
@@ -145,8 +145,8 @@ func (o *objectValidator) Validate(data interface{}) *Result {
 	// Check required properties
 	if len(o.Required) > 0 {
 		for _, k := range o.Required {
-			if _, ok := val[k]; !ok && !createdFromDefaults[k] {
-				res.AddErrors(errors.Required(o.Path+"."+k, o.In))
+			if v, ok := val[k]; !ok && !createdFromDefaults[k] {
+				res.AddErrors(errors.Required(o.Path+"."+k, o.In, v))
 				continue
 			}
 		}
