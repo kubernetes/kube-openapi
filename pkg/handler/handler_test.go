38aa46a19c76b1f4004de34b5daaa6ab798b9095
murthy95 <jlnmurthy95@gmail.com>
2023-01-26 22:01:13 +0530

    add back deprecated test

--- pkg/handler/handler_test.go
+++ pkg/handler/handler_test.go
@@ -68,6 +68,10 @@ func TestRegisterOpenAPIVersionedService(t *testing.T) {
 		{"application/json, application/com.github.proto-openapi.spec.v2.v1.0+protobuf", 200, returnedJSON},
 		{"application/com.github.proto-openapi.spec.v2.v1.0+protobuf, application/json", 200, returnedPb},
 		{"application/com.github.proto-openapi.spec.v2.v1.0+protobuf; q=0.5, application/json", 200, returnedJSON},
+		{"application/com.github.proto-openapi.spec.v2@v1.0+protobuf", 200, returnedPb},
+		{"application/json, application/com.github.proto-openapi.spec.v2@v1.0+protobuf", 200, returnedJSON},
+		{"application/com.github.proto-openapi.spec.v2@v1.0+protobuf, application/json", 200, returnedPb},
+		{"application/com.github.proto-openapi.spec.v2@v1.0+protobuf; q=0.5, application/json", 200, returnedJSON},
 	}
 
 	for _, tc := range tcs {
