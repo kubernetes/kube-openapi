cc817286cf18003b78fe7b3220464b3be72cdee2
orangeji11 <jizongyao@uniontech.com>
2023-03-20 09:17:44 +0000

    添加谷歌翻译功能，需要代理

--- pkg/common/translate.go
+++ pkg/common/translate.go
@@ -1,47 +1,49 @@
 package common
 
 import (
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"net/url"
-	"regexp"
+        "fmt"
+        "io/ioutil"
+        "net/http"
+		"net/url"
+        "html"
+        "regexp"
+		"time"
 )
 
-const YOUDAO_TRANSLATE_URL = "http://fanyi.youdao.com"
+const GOOGLE_TRANSLATE_URL = "http://translate.google.com/m?tl=zh-CN&sl=en&q="
 
 func TranslateDescription(sourceContent string) string {
 	// 内容转换为URLCode
 	Content := url.QueryEscape(sourceContent)
-	// 定义url，例如：http://fanyi.youdao.com/translate?&doctype=json&type=EN2ZH_CN&i=hello
-	url := YOUDAO_TRANSLATE_URL + "/translate?&doctype=text&type=EN2ZH_CN&i=" + Content
+	url := GOOGLE_TRANSLATE_URL + Content
+	// fmt.Printf(url)
 
-	// 获取http请求
+	// 保证访问间隔0.2s
+	time.Sleep(200 * time.Microsecond)
 	response, err := http.Get(url)
 	if err != nil {
-		fmt.Println("请求错误", err.Error())
-		// 错误返回源内容
-		return sourceContent
+			fmt.Println("请求错误", err.Error())
+			// 保守翻译，遇错返回原值
+			return sourceContent
 	}
+	// fmt.Println(response)
 
-	// 获取内容
+	// 解析Body结构中的数据
 	data, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		fmt.Println("Body 读取错误", err.Error())
-		// 错误返回源内容
-		return sourceContent
+			fmt.Println("Body 读取错误", err)
+			return sourceContent
+	}
+	// fmt.Println(string(data))
+
+	// 正则过滤除翻译内容的其他信息
+	expr := regexp.MustCompile(`(?s)class="(?:t0|result-container)">(.*?)<`)
+	result := expr.FindAllStringSubmatch(string(data), -1)
+	if len(result) == 0 {
+			return sourceContent
 	}
 
-	// 排除长度为0
-	if len(data) == 0 {
-		return sourceContent
-	}
-
-	// 解析text内容，提取翻译内容，例如：
-	// errorCode=0
-	// result=你好
-	// 匹配结果：你好
-	m := regexp.MustCompile(`[^errorCode=0][^result=*?](.*)`)
-	result := m.FindString(string(data))
-	return result
+	targetContent := html.UnescapeString(result[0][1])
+	// fmt.Println(targetContent)
+	return targetContent
 }
