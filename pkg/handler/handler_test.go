d52469dabda8948367848b6aef788b2521c1a7d7
Jeffrey Ying <jeffrey.ying86@live.com>
2023-05-23 14:42:18 -0400

    Update tests

--- pkg/handler/handler_test.go
+++ pkg/handler/handler_test.go
@@ -41,9 +41,7 @@ func TestRegisterOpenAPIVersionedService(t *testing.T) {
 
 	mux := http.NewServeMux()
 	o := NewOpenAPIService(&s)
-	if err := o.RegisterOpenAPIVersionedService("/openapi/v2", mux); err != nil {
-		t.Errorf("Unexpected error in register OpenAPI versioned service: %v", err)
-	}
+	o.RegisterOpenAPIVersionedService("/openapi/v2", mux)
 	server := httptest.NewServer(mux)
 	defer server.Close()
 	client := server.Client()
